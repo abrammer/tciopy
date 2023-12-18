901ef1ac18b4606feb26842976378dd00abd5019
Alan Brammer <alan.brammer@noaa.gov>
2023-12-18 09:48:16 -0700

    expose common read methods to top-level import

--- src/tciopy/__init__.py
+++ src/tciopy/__init__.py
@@ -1,3 +1,5 @@
 from tciopy.atcf.abdeck import read_adeck, read_bdeck
+from tciopy.atcf.fdeck import read_fdeck
+from tciopy.cxml.reader import read_cxml
 
-__all__ = ["read_adeck", "read_bdeck"]
+__all__ = ["read_adeck", "read_bdeck", "read_fdeck", "read_cxml"]
