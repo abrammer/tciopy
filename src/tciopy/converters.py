8b3d28c0063e287756f351d35d212b6bf81fd717
Alan Brammer <alan.brammer@noaa.gov>
2025-08-11 17:36:13 -0600

    big simplifications and speed ups changing to polars. changes to abdeck and edeck so far.

--- src/tciopy/converters.py
+++ src/tciopy/converters.py
@@ -8,6 +8,26 @@
 
 import numpy as np
 import pandas as pd
+import polars as pl
+
+
+def tolatlon(dataframe: pl.DataFrame,
+            lat: str = 'lat',
+            lon: str = 'lon',
+            scale:float = 0.1) -> pl.DataFrame:
+    """Convert lat/lon columns to numerical values in degrees"""
+    latnum = pl.col(lat).str.strip_chars('NS ').cast(pl.Float64, strict=False) * scale  # 64 is overkill but 32 creating floating point weirdness
+    lonnum = pl.col(lon).str.strip_chars('EW ').cast(pl.Float64, strict=False) * scale
+    return dataframe.with_columns([
+            pl.when(pl.col(lat).str.ends_with('S'))
+            .then(latnum * -1.0)
+            .otherwise(latnum),
+            pl.when(pl.col(lon).str.ends_with('W'))
+            .then(lonnum * -1.0)
+            .otherwise(lonnum)
+    ])
+
+
 
 
 class StringColumn(list):
