350036a2d444fe48a57fbc5a181748f89df83605
Alan Brammer <alan.brammer@noaa.gov>
2025-11-17 16:06:10 -0700

    basic cxml reader in polars

--- src/tciopy/__init__.py
+++ src/tciopy/__init__.py
@@ -1,5 +1,6 @@
 from tciopy.atcf.abdeck import read_adeck, read_bdeck, read_adecks, read_bdecks
 from tciopy.atcf.fdeck import read_fdeck
+from tciopy.atcf.edeck import read_edeck
 from tciopy.cxml.reader import read_cxml
 
-__all__ = ["read_adeck", "read_bdeck", "read_fdeck", "read_cxml", "read_adecks", "read_bdecks"]
+__all__ = ["read_adeck", "read_bdeck", "read_fdeck","read_edeck", "read_cxml", "read_adecks", "read_bdecks"]
